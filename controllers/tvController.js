27ea462c78e9455822452853028a464d1e903de0
jrodriguez <jrodriguez@bdgsa.net>
2017-11-22 19:53:50 -0600

    cambio a promesas

--- controllers/tvController.js
+++ controllers/tvController.js
@@ -24,31 +24,22 @@ const tvShows = [
         pais: 'MEX'
     }
 ];
-const toDo = (callback) => {
-    TVShow.find((err, tvShows) => {
-        if (err) {
-            return callback({ error: err }, null);
-        }
-        callback(null, tvShows);
 
+const todo = (tvshows) => {
+    return new Promise((resolve, reject) => {
+        if (tvshows.length < 0) {
+            return reject({ error: 'No hay datos' });
+        }
+        return resolve({ data: tvshows.length });
     });
+    //return TVShow.find();
 }
 
 obj.getArray = (req, res, next) => {
-    //res.send(tvShows);
-    /*TVShow.find((err, tvShows) => {
-        if (err) {
-            res.send({ error: err });
-        } else {
-            res.send(tvShows);
-        }
-    });*/
-    toDo((error, result) => {
-        if (error) {
-            return res.send({ error: error });
-        }
-        res.send(result);
-    });
+    TVShow.find()
+        .then(tvShows => res.send(tvShows))
+        .catch(err => res.send({ error: err })
+        );
 };
 
 obj.postArray = (req, res, next) => {
@@ -58,66 +49,28 @@ obj.postArray = (req, res, next) => {
             anio: req.body.anio,
             pais: req.body.pais
         });
-
-    data.save((err, result) => {
-        if (err) {
-            return res.send({ error: err });
-        } else {
-            let a = algo();
-            if (a.error) {
-                return res.send({ error: a.error });
-            } else {
-                res.send(a);
-            }
-            return res.send();
-        }
-    });
+    data.save()
+        .then(resutl => res.send())
+        .catch(err => res.send({ error: err })
+        );
 }
+
 obj.getById = (req, res, next) => {
-    TVShow.findById(req.params.id, (error, result) => {
-        if (error) {
-            return res.send({ error: error });
-        } else {
-            if (result) {
-                return res.send(result);
-            } else {
-                return res.send({ error: `Show: ${req.params.id},no encontrado}` });
-            }
-
-        }
-    });
+    TVShow.findById(req.params.id)
+        .then(result => (result ? res.send(result) : res.send({ error: `Show: ${req.params.id},no encontrado}` })))
+        .catch(err => res.send({ error: err }));
 }
+
 obj.deleteTvShow = (req, res, next) => {
-    TVShow.findByIdAndRemove(req.params.id, (error, result) => {
-        if (error) {
-            return res.send({ error: error });
-        } else {
-            res.send(result);
-        }
-    });
-}
-obj.updateTvShow = (req, res, next) => {
-    TVShow.findByIdAndUpdate(req.params.id, req.body, (error, result) => {
-        if (error) {
-            return res.send({ error: error });
-        } else {
-            res.send(result);
-        }
-    });
-    /*let indexTvShow = tvShows.findIndex((tvShow) => tvShow.id === Number.parseInt(req.params.id));
-    if (indexTvShow < 0) {
-        return res.send({ error: `Show: ${req.params.id},no encontrado}` });
-    }
-    //obtengo el objeto en la posicion según el id
-    let tvShow = tvShows[indexTvShow];
-    //actualizo las propiedades del objeto, manteniendo el id
-    tvShow.anio = req.body.anio;
-    tvShow.pais = req.body.pais;
-    tvShow.titulo = req.body.titulo;
-    //substituyo el objeto en la posición por el que contiene los cambios realizados
-    tvShows[indexTvShow] = tvShow;*/
-    //return res.send(tvShows);
+    TVShow.findByIdAndRemove(req.params.id)
+        .then(result => res.send(result))
+        .catch(err => res.send({ error: err }));
 }
 
+obj.updateTvShow = (req, res, next) => {
+    TVShow.findByIdAndUpdate(req.params.id, req.body)
+        .then(result => res.send(result))
+        .catch(err => res.send({ error: error }));
+}
 
 module.exports = obj;
